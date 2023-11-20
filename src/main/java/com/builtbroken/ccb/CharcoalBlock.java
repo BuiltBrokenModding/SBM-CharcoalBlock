c679acf054a2f7022bb67391389e03cb883a8e68
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-11-20 15:13:50 +0100

    feature: update to 1.20

--- src/main/java/com/builtbroken/ccb/CharcoalBlock.java
+++ src/main/java/com/builtbroken/ccb/CharcoalBlock.java
@@ -12,9 +12,8 @@ import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.world.level.material.MaterialColor;
-import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -32,7 +31,7 @@ public class CharcoalBlock
 	public static final String MODID = "sbmcharcoalblock";
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-	public static final RegistryObject<Block> CHARCOAL_BLOCK = BLOCKS.register("charcoal_block", () -> new Block(Block.Properties.of(Material.STONE, MaterialColor.COLOR_BLACK)
+	public static final RegistryObject<Block> CHARCOAL_BLOCK = BLOCKS.register("charcoal_block", () -> new Block(Block.Properties.of().mapColor(MapColor.COLOR_BLACK)
 			.strength(5.0F, 10.0F)
 			.sound(SoundType.STONE)
 			.requiresCorrectToolForDrops()));
@@ -54,9 +53,9 @@ public class CharcoalBlock
 	}
 
 	@SubscribeEvent
-	public static void onCreativeModeTabBuildContents(CreativeModeTabEvent.BuildContents event)
+	public static void onCreativeModeTabBuildContents(BuildCreativeModeTabContentsEvent event)
 	{
-		if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS)
+		if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
 			event.getEntries().putAfter(new ItemStack(Blocks.COAL_BLOCK), new ItemStack(CHARCOAL_BLOCK_ITEM.get()), TabVisibility.PARENT_AND_SEARCH_TABS);
 	}
 }
